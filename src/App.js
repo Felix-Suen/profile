5b3cc6e4c08b3a67f39ae4ed75d3a7f0bcc2309f
Felix Suen <felix.suen2000@gmail.com>
2019-12-09 14:51:13 -0500

    day difference

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  Daynum () {
+    var currentDate = new Date();
+    var firstDate = new Date("09/21/2018");
+    var time_difference = currentDate.getTime() - firstDate.getTime();
+    var day_difference = Math.round(time_difference / (1000 * 3600 * 24));
+    var date = currentDate.getFullYear() + '-' + (currentDate.getMonth()+1) + '-' + currentDate.getDate();
+    return (
+    <p>{day_difference}</p>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>{this.Daynum()}</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
