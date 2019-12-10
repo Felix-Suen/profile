5f0eaf9a2f45fc2edf5cef0af15e2c6b0721e4fe
Felix-Suen <felix.suen2000@gmail.com>
2019-12-09 23:36:17 -0500

    change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import Snow from 'react-snow-effect';
 import './App.css';
 
@@ -8,7 +7,7 @@ class App extends React.Component {
     var currentDate = new Date();
     var firstDate = new Date("09/21/2018");
     var time_difference = currentDate.getTime() - firstDate.getTime();
-    var day_difference = Math.round(time_difference / (1000 * 3600 * 24));
+    var day_difference = Math.floor(time_difference / (1000 * 3600 * 24));
     return day_difference;
   }
 
